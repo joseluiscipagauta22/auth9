32b312e88670bd731f44f14aedc61803c197ae23
JoseCipagauta <jose.arbelaez2229@gmail.com>
2026-07-28 11:43:51 -0500

    ajustes render neon

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,6 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import config from './config';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -19,15 +18,21 @@ import config from './config';
       validationSchema: Joi.object({
         PORT: Joi.number().default(3000),
         NODE_ENV: Joi.string().valid('dev', 'stg', 'prod').default('dev'),
-        POSTGRES_DB: Joi.string().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
+        
+        // Permite la URL completa de Neon / Render
+        DATABASE_URL: Joi.string().optional(),
+        
+        // Pasan de ser .required() a opcionales para que no rompa en Render
+        POSTGRES_DB: Joi.string().optional(),
+        POSTGRES_USER: Joi.string().optional(),
+        POSTGRES_PASSWORD: Joi.string().optional(),
         POSTGRES_PORT: Joi.number().default(5432),
-        POSTGRES_HOST: Joi.string().required(),
-        JWT_SECRET: Joi.string().required(),
-        JWT_EXPIRES_IN: Joi.number().required(),
-        CORS_ORIGINS: Joi.string().required(),
-      }),
+        POSTGRES_HOST: Joi.string().optional(),
+        
+        JWT_SECRET: Joi.string().optional(),
+        JWT_EXPIRES_IN: Joi.number().optional(),
+        CORS_ORIGINS: Joi.string().optional(),
+      }).or('DATABASE_URL', 'POSTGRES_HOST'), // Exige que al menos exista DATABASE_URL o POSTGRES_HOST
     }),
     DatabaseModule,
     UsersModule,
